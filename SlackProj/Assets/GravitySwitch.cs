c0004588fd708e75d5b26f411750364fc38cd4d0
Tyler Kaminsky <tylerkaminsky@gmail.com>
2015-09-20 16:20:25 -0400

    Push starting files

---
+++ SlackProj/Assets/GravitySwitch.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GravitySwitch : MonoBehaviour {
+
+    // Use this for initialization
+    public GameObject prnt;
+    public Rigidbody body;
+    void Start () {
+        prnt = GameObject.Find("CubeParent");
+        body = GetComponent<Rigidbody>();
+        
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //print(prnt.transform.rotation.eulerAngles.x);
+
+        if (prnt.transform.rotation.eulerAngles.x > 2 && prnt.transform.rotation.eulerAngles.x < 358)
+        {
+            body.useGravity = true;
+        }
+	
+	}
+}
---
+++ SlackProj/Assets/RotateScript.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateScript : MonoBehaviour {
+
+    // Use this for initialization
+    float tilt;
+    float recover;
+    void Start () {
+         tilt = Random.Range(.1f, -.1f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        recover = transform.rotation.eulerAngles.x;
+        print(recover);
+
+        transform.Rotate(tilt, 0, 0);
+
+        if (Input.GetKey("left")) {
+            tilt = -.2f;
+            transform.Rotate(tilt, 0, 0);
+            
+        }
+
+        if(Input.GetKey("right"))
+        {
+            tilt = .2f;
+            transform.Rotate(tilt, 0, 0);
+            
+        }
+
+        if (Input.GetKeyDown("space"))
+        {
+            print("RECOVERED");
+            print(recover);
+            print("END");
+            transform.Rotate(-recover*3 , 0, 0);
+            
+
+        }
+	}
+}
